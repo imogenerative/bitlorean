9cf28ae86e97a87f9c0abd618530bb7d281f29e7
Imogen Fulton <imogen@dfdt.me>
2018-02-10 18:38:25 +1100

    it's a shitty heroku app now

--- bitlorean.py
+++ bitlorean.py
@@ -2,7 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 from collections import namedtuple
 
+from flask import Flask, render_template
+
+bitlorean = Flask(__name__)
+
 BEC = 'https://digiconomist.net/bitcoin-energy-consumption'
+
+def trips(jigawatts):
+    can = ""
+
+    if jigawatts > 1.21:
+        can = "YES"
+    else:
+        can = "NO"
+
+    return can
+
+def marty(jigawatts):
+    mcfly = ""
+
+    if jigawatts > 1.21:
+        mcfly =  "That is enough for Marty McFly to travel Back to the Future %d times." % int(jigawatts / 1.21)
+    else:
+        mcfly = "That is not enough for Marty McFly to travel Back to the Future."
+
+    return mcfly
 
 def bitLorean():
     source = requests.get(BEC)
@@ -10,7 +34,7 @@ def bitLorean():
 
     def scrapeSoup(soup):
         jigaTuple = namedtuple('jigaTuple', 'Watts hashrate')
-        
+
         impliedWatts = soup.find('tr', {'id': 'table_3_row_5'})
         impliedWatts = impliedWatts.find_all('td')[1].get_text()
         Watts = float(impliedWatts)  # Watts is never watts
@@ -20,7 +44,7 @@ def bitLorean():
         hashrate = float(totalHashrate) * (10 ** 6)  # convert PH/s to GH/s
 
         return jigaTuple(Watts=Watts, hashrate=hashrate)
-    
+
     jigaWatts = scrapeSoup(soup)
 
     Watts = jigaWatts.Watts
@@ -30,4 +54,14 @@ def bitLorean():
 
     return jigaBitcoins
 
-print('Bitcoin consumes %s jigaWatts of power...' % bitLorean())
+#print('Bitcoin consumes %s jigaWatts of power...' % bitLorean())
+
+@bitlorean.route('/')
+def app():
+    jigawatts = bitLorean()
+
+    return render_template("index.html", jigawatts=jigawatts, can=trips(jigawatts), mcfly=marty(jigawatts))
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
