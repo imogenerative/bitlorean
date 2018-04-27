8b4e6475abb44d5ae6c79241a94bb9728ba284ea
Imogen Fulton <imogen@dfdt.me>
2018-04-27 15:50:39 +1000

    digiconomist changed a thing so I had to

--- bitlorean.py
+++ bitlorean.py
@@ -35,11 +35,11 @@ def bitLorean():
     def scrapeSoup(soup):
         jigaTuple = namedtuple('jigaTuple', 'Watts hashrate')
 
-        impliedWatts = soup.find('tr', {'id': 'table_3_row_5'})
+        impliedWatts = soup.find('tr', {'id': 'table_3_row_6'})
         impliedWatts = impliedWatts.find_all('td')[1].get_text()
         Watts = float(impliedWatts)  # Watts is never watts
 
-        totalHashrate = soup.find('tr', {'id': 'table_3_row_6'})
+        totalHashrate = soup.find('tr', {'id': 'table_3_row_7'})
         totalHashrate = totalHashrate.find_all('td')[1].get_text().replace(',', '')
         hashrate = float(totalHashrate) * (10 ** 6)  # convert PH/s to GH/s
 
